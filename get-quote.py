4a4c6f1e0be48f8f5468bd9e5147f04ed8d3e81d
awchjimmy <awchjimmy@gmail.com>
2022-01-11 15:39:42 +0800

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,9 +1,15 @@
+import random
+
 def primary():
+    
+
     f = open("quotes.txt","r",encoding="utf-8")
     quotes = f.readlines()
     f.close()
 
-    print(quotes[13])
+    last = 13
+    rnd = random.randint(0, last)
+    print(quotes[rnd])
 
 if __name__== "__main__":
     primary()
